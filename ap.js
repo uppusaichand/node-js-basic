7dc604aa20184a0cf19dc243a7c6b095d07c7f4b
unknown <you@example.com>
2021-12-08 09:05:22 +0530

    routing

--- ap.js
+++ ap.js
@@ -2,15 +2,17 @@ const http = require('http')
 const url = require('url')
 const server = http.createServer((req,res)=>{
     const path = req.url
-    if (path==='/profile'||'/'){
+    if (path==='/profile'){
         res.end("good res")
     }else {
-        res.writeHead(404,{
+        res.writeHead(404),{
             'content-type':'text/html',
-            'my-own-header':'hello',})
+            'my-own-header':'hello'}
+            res.end('<h1>page not found</h1>')
+            
         }
-        res.end("<h1>page not found</h1>")
+        
 })
-server.listen(9000,'127.0.0.1',()=>{
+server.listen(5000,'127.0.0.1',()=>{
     console.log("this is a server")
 })
